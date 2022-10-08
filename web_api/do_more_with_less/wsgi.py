04554fb5db55b73a4622f04f281713271319bc8d
Mariusz Sobczak <mariuszsobczak87@gmail.com>
2022-10-08 16:12:15 +0200

    chore: add isort, black, flake8

--- web_api/do_more_with_less/wsgi.py
+++ web_api/do_more_with_less/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'do_more_with_less.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "do_more_with_less.settings")
 
 application = get_wsgi_application()
--- web_api/entrypoint.sh
+++ web_api/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#dockerize -wait tcp://$DB_HOST:$DB_PORT -wait tcp://$ES_HOST:$ES_PORT -timeout 45s
+dockerize -wait tcp://$DB_HOST:$DB_PORT -timeout 45s
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
