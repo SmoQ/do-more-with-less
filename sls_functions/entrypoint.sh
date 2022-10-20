db594db721b0531a0d171e879afbc0a2ac90ff25
Mariusz Sobczak <mariuszsobczak87@gmail.com>
2022-10-20 10:26:04 +0200

    chore: start dynamodb sls

--- sls_functions/entrypoint.sh
+++ sls_functions/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-tail -f /dev/null
+aws configure set default.aws_secret_access_key dummy_secret_key \
+    && aws configure set default.aws_access_key_id dummy_access_key_id
+npx sls dynamodb start --config=serverless.yml --stage=local &
+npx sls offline --config=serverless.yml --stage=local
